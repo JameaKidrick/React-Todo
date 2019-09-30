767edb802cf09eabd24550504cfef34cf28a10a6
JameaKidrick <50430635+JameaKidrick@users.noreply.github.com>
2019-09-30 17:41:14 -0400

    added line through on completed tasks

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import TodoList from './components/TodoComponents/TodoList'
+import './components/TodoComponents/Todo.css'
 
 // STEP 1: MAKE LIST
 const todoList = [
--- src/components/TodoComponents/Todo.js
+++ src/components/TodoComponents/Todo.js
@@ -5,7 +5,7 @@ const Todo = props => {
   return(
     // ALLOWS DEV TO VISUALIZE COMPLETION IN DEV TOOLS USING CLASS
     <div 
-    className={`task ${props.item.completed ? ' completed' : ' not completed'}`}
+    className={`task ${props.item.completed ? ' completed' : ' '}`}
     // STEP 10: CREATE ONCLICK TO TOGGLE TASK BASED ON ID (WHICH WILL BE COMPARED TO ID OF ARRAY LIST USING THE IF STATEMENT IN APP.JS)
     onClick={()=>props.toggleTask(props.item.id)}
     >
